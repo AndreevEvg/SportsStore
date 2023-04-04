c747c3ac972c02b0d0be4f4053246716240ba888
Evgeniy Andreev <7253968@gmail.com>
2023-04-04 23:02:38 +0300

    Add Model/ViewModels/ProductsListViewModel

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers;
 
@@ -15,10 +16,18 @@ public class ProductController : Controller
     }
 
     public ViewResult List(int productPage = 1) 
-        => View(repository.Products
-            .OrderBy(p => p.ProductID)
-            .Skip((productPage - 1) * PageSize)
-            .Take(PageSize));
+        => View(new ProductsListViewModel 
+        {
+            Products = repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize),
+            PagingInfo = new PagingInfo {
+                CurrentPage = productPage,
+                ItemsPerPage = PageSize,
+                TotalItems = repository.Products.Count()
+                }
+        });
 
 
 }
